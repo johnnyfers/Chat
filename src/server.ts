0a09c3f790352dbd66259579c0dc5c69777def92
joao <joaovlima07@hotmail.com>
2021-04-23 20:25:49 -0300

    nlw day5

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { http } from './http';
-import './webSocket/client'
+import './webSocket/client';
+import './webSocket/admin';
 
 http.listen(3333, () => console.log('running on port 3333'))
--- src/webSocket/client.ts
+++ src/webSocket/client.ts
@@ -55,12 +55,30 @@ io.on("connect", (socket) => {
             user_id
         })
 
+        const allMessages = await messagesService.listByUser(user_id);
+
+        socket.emit('client_list_all_msgs', allMessages);
+
     });
+
+    socket.on('client_send_message_to_admin', async (params)=>{
+        const { text, socket_admin_id } = params;
+
+        const socket_id = socket.id
+
+        const {user_id} = await connectionsServices.findByUserId(socket_id)
+
+        const message = await messagesService.create({
+            text,
+            user_id
+        })
+
+
+        io.to(socket_admin_id).emit('admin_receive_message',{
+            message,
+            socket_id
+        })
+    })
 
 });
 
-
-
-/*
-
-*/
